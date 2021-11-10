a44dd3f407b39f4a59b02ad863cc408fd95dc8fe
Javier <alvmurjav@gmail.com>
2021-11-10 09:49:37 +0100

    Se añadió mi nombre, localidad,residencia

--- App.java
+++ App.java
@@ -1,9 +1,10 @@
 public class App {
   
   public static void main(String[] args) {
-    System.out.println("Contenido 2a");
-    System.out.println("Contenido 3");
-    System.out.println("Contenido 2b");
-  }
+    System.out.println("Javier");
+    System.out.println("4/11/2021");
+    System.out.println("León");
+    System.out.println("León ciudad");
+ }
   
 }
